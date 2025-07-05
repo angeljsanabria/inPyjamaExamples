3c9393ca9544e711e872444582ff28109cfe97de
angeljsanabria <129133010+angeljsanabria@users.noreply.github.com>
2025-07-05 15:44:01 +0000

    add, check win

--- tic.c
+++ tic.c
@@ -1,5 +1,5 @@
 /*
-CHEATSHEET
+CHEAT SHEET
 gcc tic.c -o app
 ./app
 
@@ -26,6 +26,14 @@ gcc tic.c -o app && ./app
 #define P2_CENTER 'X'
 #define MOVIMIENTOS 9
 #define INTENTOS 3
+#define WIN1    (3+5+7)
+#define WIN2    (11+13+17)
+#define WIN3    (19+23+29)
+#define WIN4    (3+11+10)
+#define WIN5    (5+13+23)
+#define WIN6    (7+17+29)
+#define WIN7    (3+13+29)
+#define WIN8    (19+13+7)
 
 void _lineah(){
     int hsize = (int)(XPER_CELL*X_CELL);
@@ -41,6 +49,7 @@ void _tablero (int* mv){
 
     int move = 0;
     int region, yregion;
+    system("clear");  
     _lineah();
     SALTO_LINEA();
     for (region = 0; region < REGIONES; region++){
@@ -73,63 +82,108 @@ void _tablero (int* mv){
         }
     }
 
-    SALTO_LINEA();
-
-    
+    SALTO_LINEA(); 
     return;
 }
+
 
 int _isNumber(int n){
     if ((n < 0x30) || (n>0x39)) return 0;
     return 1;
 }
 
+
 int get_play(int player){
     
     int retries = 0;
     do{
         retries++;
-        printf("Player %d Selecciona tu jugada: ", (player+1));
         int c = getchar(); // Read a character
         if (_isNumber(c)) {
             // Convert the character digit to its integer value
             // by subtracting the ASCII value of '0'
-            int number = c - '0' -1;
-            return number;
+            int number = c - '0';
+            return (number -1);
         } else if(c != 10){
             printf("Invalido: No es un numero\n");
             if(retries > INTENTOS)  return 0xFFFF;
         }
-
     }while(1);
-
 }
 
+
+void _add_points(int *points, int ip, int play){
+    points[ip] = play+1;
+    return;
+}
+
+int _check_win(int *m, int target){
+
+    if      ((m[0] == target) && (m[1] == target) && (m[2] == target))  return 1;
+    else if ((m[3] == target) && (m[4] == target) && (m[5] == target))  return 1;
+    else if ((m[6] == target) && (m[7] == target) && (m[8] == target))  return 1;
+
+    else if ((m[0] == target) && (m[4] == target) && (m[8] == target))  return 1;
+    else if ((m[6] == target) && (m[4] == target) && (m[2] == target))  return 1;
+
+    else if ((m[0] == target) && (m[3] == target) && (m[6] == target))  return 1;
+    else if ((m[1] == target) && (m[4] == target) && (m[7] == target))  return 1;
+    else if ((m[2] == target) && (m[5] == target) && (m[8] == target))  return 1;
+
+    return 0;
+}
+
+void _won_msg(int player){
+    printf("\n#### WINNER ###\n");
+    printf("-- PLAYER %d --\n", (player+1));
+    printf("##############\n");
+}
 
 void main(){
     // init moves
     int moves[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
     int player = 0;
     int play;
+    int plays = 0;
     // tablero
-    for (int plays = 0; plays < MOVIMIENTOS; plays++){
 
+    do{   
         _tablero(moves);
+        
+        printf("\nPlayer %d Selecciona tu jugada: ", (player+1));
         play = get_play(player);
         if(play > MOVIMIENTOS){
             printf("*******/n FINALIZADO POR ERROR DE JUGADOR /n ********");
             return;
         }
+        
+        // ver si ya se hizo la jugada
+        if((moves[play] == P1_CENTER) || (moves[play] == P2_CENTER)){
+            printf("Jugada repetida! \n");
+            continue;  
+        } 
+            
         if(!player){
             moves[play] = P1_CENTER;
-            player++;
+            if(_check_win(&moves[0], P1_CENTER)){
+                _tablero(moves);
+                _won_msg(player);
+                return;
+            }
         }else{
             moves[play] = P2_CENTER;
-            player = 0;
-
+            if(_check_win(&moves[0], P2_CENTER)){
+                _tablero(moves);
+                _won_msg(player);
+                return;
+            }
         }
 
-    }
+        if(!player) player = 1;
+        else        player = 0;
+            
+        plays++;
+    }while(plays < MOVIMIENTOS);
     
     return;
 }
